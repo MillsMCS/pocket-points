d170eabead601d22d7924f1e7b6bc54590c0d0ac
ajkwak <ajkwak@users.noreply.github.com>
2014-04-28 18:57:51 -0700

    Refactor code such that java CamelCase and xml snake_case are consistent

--- PocketPoints/src/edu/mills/cs180a/pocketpoints/MainActivity.java
+++ PocketPoints/src/edu/mills/cs180a/pocketpoints/MainActivity.java
@@ -12,8 +12,8 @@ import android.view.MenuItem;
  * @author chingmyu@gmail.com (Ching Yu)
  */
 public class MainActivity extends Activity
-	implements ClassListFragment.OnStudentSelectedListener,
-	EditClassListFragment.OnEditStudentSelectedListener {
+	implements ClasslistFragment.OnStudentSelectedListener,
+	EditClasslistFragment.OnEditStudentSelectedListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
