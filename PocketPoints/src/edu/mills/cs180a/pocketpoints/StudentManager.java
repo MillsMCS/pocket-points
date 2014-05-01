9d838c6c2d79ceeeda509f46c978f6ff0656d725
ajkwak <ajkwak@users.noreply.github.com>
2014-05-01 14:59:04 -0700

    Fix bug in StudentManager where an error was being logged if the return was correct, but not being logged in the error case

--- PocketPoints/src/edu/mills/cs180a/pocketpoints/StudentManager.java
+++ PocketPoints/src/edu/mills/cs180a/pocketpoints/StudentManager.java
@@ -21,8 +21,7 @@ import edu.mills.cs180a.pocketpoints.StudentSQLiteOpenHelper.StudentCursor;
  * @author ajkwak@users.noreply.github.com (AJ Parmidge)
  */
 // Modeled after the RunManager class from the RunTracker app in "Android
-// Programming: The Big Nerd
-// Ranch Guide".
+// Programming: The Big Nerd Ranch Guide".
 public class StudentManager {
     private static final String TAG = "StudentManager";
     private static StudentManager sStudentManager;
@@ -30,12 +29,10 @@ public class StudentManager {
     private StudentSQLiteOpenHelper mHelper;
 
     /**
-     * Get the singleton instance of {@code StudentManager}. If no instance
-     * currently exists, create an instance using the singleton application
-     * context provided by the given context.
+     * Get the singleton instance of {@code StudentManager}. If no instance currently exists, create
+     * an instance using the singleton application context provided by the given context.
      *
-     * @param context
-     *            the context to use
+     * @param context the context to use
      * @return the current instance of {@StudentManager}
      */
     public static synchronized StudentManager get(Context context) {
@@ -54,9 +51,8 @@ public class StudentManager {
      * <b><i> PLEASE NOTE </i></b> that this method exists only for testing the
      * {@code StudentManager}. Please use it for NO other purpose.
      *
-     * @param context
-     *            the context of the test for which to create this
-     *            {@code StudentManager} instance
+     * @param context the context of the test for which to create this {@code StudentManager}
+     *        instance
      * @return the {@code StudentManager} test instance for the given context
      */
     public static StudentManager getTestInstance(Context context) {
@@ -68,15 +64,13 @@ public class StudentManager {
     }
 
     /**
-     * Adds the given student to the database. If the student was successfully
-     * added, changes the ID of the student to reflect the student's unique ID
-     * in the database. If the student was not added, sets the ID of the student
-     * to an invalid value.
+     * Adds the given student to the database. If the student was successfully added, changes the ID
+     * of the student to reflect the student's unique ID in the database. If the student was not
+     * added, sets the ID of the student to an invalid value.
      *
-     * @param student
-     *            the student to add
-     * @return {@code true} if the student was successfully created in the
-     *         database; otherwise {@code false}
+     * @param student the student to add
+     * @return {@code true} if the student was successfully created in the database; otherwise
+     *         {@code false}
      */
     public boolean createStudent(Student student) {
         return mHelper.insertStudent(student) > -1;
@@ -110,20 +104,17 @@ public class StudentManager {
     /**
      * Retrieves the student with the given ID from the database.
      *
-     * @param id
-     *            the ID of the student to get
-     * @return the student with the given ID, or {@code null} if there is no
-     *         student with the given ID in the database
+     * @param id the ID of the student to get
+     * @return the student with the given ID, or {@code null} if there is no student with the given
+     *         ID in the database
      */
     public Student getStudent(long id) {
         StudentCursor studentCursor = mHelper.queryStudent(id);
         Student student = null;
         if (studentCursor.moveToFirst()) {
             student = studentCursor.getStudent();
-            if (studentCursor.isLast()) {
-                Log.e(TAG,
-                        "Database cursor returned more than 1 result for student with id = "
-                                + id);
+            if (!studentCursor.isLast()) {
+                Log.e(TAG, "Cursor returned more than 1 result for student with id = " + id);
             }
         }
         studentCursor.close();
@@ -133,10 +124,9 @@ public class StudentManager {
     /**
      * Updates the values associated with the given student in the database.
      *
-     * @param student
-     *            the student to update
-     * @return {@code true} if the student was successfully updated in the
-     *         database; otherwise {@code false}
+     * @param student the student to update
+     * @return {@code true} if the student was successfully updated in the database; otherwise
+     *         {@code false}
      */
     public boolean updateStudent(Student student) {
         return mHelper.updateStudent(student) != 0;
@@ -145,10 +135,9 @@ public class StudentManager {
     /**
      * Deletes the student with the given ID from the database.
      *
-     * @param id
-     *            the ID of the student to delete
-     * @return {@code true} if the student was successfully deleted from the
-     *         database; otherwise {@code false}
+     * @param id the ID of the student to delete
+     * @return {@code true} if the student was successfully deleted from the database; otherwise
+     *         {@code false}
      */
     public boolean deleteStudent(long id) {
         return mHelper.deleteStudent(id) != 0;
