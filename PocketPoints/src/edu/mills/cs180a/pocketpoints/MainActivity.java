bfff883bd478e4717ce738e039bc56cd0d6b378b
Ching Yu <chingmyu@gmail.com>
2014-05-02 14:52:45 -0700

    Fixed bug where app crashes when edit class menu option is selected.

--- PocketPoints/src/edu/mills/cs180a/pocketpoints/MainActivity.java
+++ PocketPoints/src/edu/mills/cs180a/pocketpoints/MainActivity.java
@@ -67,8 +67,10 @@ public class MainActivity extends Activity implements ClasslistFragment.OnStuden
             // Tell the EditStudentFragment that it is creating a new student.
             mEditStudentFragment.setStudent(Student.INVALID_ID);
             return true;
+
         case R.id.menu_item_done_edit:
             mFragmentManager.popBackStack();
+            return true;
 
         default:
             return super.onOptionsItemSelected(item);
